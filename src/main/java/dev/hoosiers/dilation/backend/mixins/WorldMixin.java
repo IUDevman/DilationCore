a0761e253d8513ca0f410dff953aaf126e59a76f
IUDevman <58370012+IUDevman@users.noreply.github.com>
2026-03-16 19:14:41 -0500

    Update Gassy

--- src/main/java/dev/hoosiers/dilation/backend/mixins/WorldMixin.java
+++ src/main/java/dev/hoosiers/dilation/backend/mixins/WorldMixin.java
@@ -1,11 +1,13 @@
 package dev.hoosiers.dilation.backend.mixins;
 
 import dev.hoosiers.dilation.backend.events.LightValueEvent;
+import dev.hoosiers.dilation.backend.events.SmokeEmoteEvent;
 import dev.hoosiers.dilation.imp.LinkedMethods;
 import net.minecraft.common.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -27,6 +29,22 @@ public final class WorldMixin implements LinkedMethods {
 
         if (lightValueEvent.isCancelled()) {
             cir.setReturnValue(1000f);
+        }
+    }
+
+    @Inject(method = "playAuxSFX", at = @At("HEAD"), cancellable = true)
+    public void playAusSFX(int type, double x, double y, double z, int payload, CallbackInfo ci) {
+        if (this.failsNullCheck()) {
+            return;
+        }
+
+        if (payload == 6) {
+            SmokeEmoteEvent smokeEmoteEvent = new SmokeEmoteEvent(x, y, z);
+            this.getEventHandler().call(smokeEmoteEvent);
+
+            if (smokeEmoteEvent.isCancelled()) {
+                ci.cancel();
+            }
         }
     }
 }
--- src/main/java/dev/hoosiers/dilation/feature/hacks/Gassy.java
+++ src/main/java/dev/hoosiers/dilation/feature/hacks/Gassy.java
@@ -1,7 +1,9 @@
 package dev.hoosiers.dilation.feature.hacks;
 
+import dev.hoosiers.dilation.backend.events.SmokeEmoteEvent;
 import dev.hoosiers.dilation.imp.Category;
 import dev.hoosiers.dilation.imp.Hack;
+import dev.hoosiers.dilation.imp.event.EventTarget;
 import net.minecraft.common.networking.Packet92Emote;
 import org.lwjgl.input.Keyboard;
 
@@ -22,6 +24,18 @@ public final class Gassy extends Hack {
             this.sendPacket(new Packet92Emote(6));
         } else {
             this.getWorld().playAuxSFX(2050, this.getPlayer().posX, this.getPlayer().posY, this.getPlayer().posZ, 6);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @EventTarget
+    public void onSmokeEmoteEvent(SmokeEmoteEvent smokeEmoteEvent) {
+
+        if (this.getMinecraft().gameSettings.thirdPersonView == 0) {
+
+            if (this.getPlayer().posX == smokeEmoteEvent.x && this.getPlayer().posY == smokeEmoteEvent.y && this.getPlayer().posZ == smokeEmoteEvent.z) {
+                smokeEmoteEvent.setCancelled(true);
+            }
         }
     }
 }
